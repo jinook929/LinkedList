dfdcca603ab44aeb7ea6061ced5ca5225da3bfdc
jinook929 <jinook929@gmail.com>
2019-12-11 20:38:30 -0500

    Test ArrayList version

--- src/academy/learnprogramming/Demo_ArrayList.java
+++ src/academy/learnprogramming/Demo_ArrayList.java
@@ -1,28 +1,33 @@
 package academy.learnprogramming;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Demo_ArrayList {
 
     public static void main(String[] args) {
         ArrayList<String> placesToVisit = new ArrayList<>();
-        placesToVisit.add("_Sydney");
-        placesToVisit.add("_Melbourne");
-        placesToVisit.add("_Brisbane");
-        placesToVisit.add("_Perth");
-        placesToVisit.add("_Canberra");
-        placesToVisit.add("_Adelaide");
-        placesToVisit.add("_Darwin");
+        addInOrder(placesToVisit, "_Sydney");
+        addInOrder(placesToVisit, "_Melbourne");
+        addInOrder(placesToVisit, "_Brisbane");
+        addInOrder(placesToVisit, "_Perth");
+//        placesToVisit.add("_Sydney");
+//        placesToVisit.add("_Melbourne");
+//        placesToVisit.add("_Brisbane");
+//        placesToVisit.add("_Perth");
+//        placesToVisit.add("_Canberra");
+//        placesToVisit.add("_Adelaide");
+//        placesToVisit.add("_Darwin");
 
         printList(placesToVisit);
 
-        placesToVisit.add(1, "_Alice Springs");
+        addInOrder(placesToVisit, "_Seoul");
+//        placesToVisit.add(1, "_Alice Springs");
         printList(placesToVisit);
 
         placesToVisit.remove(4);
         printList(placesToVisit);
+
+        visit(placesToVisit);
 
     }
 
@@ -53,5 +58,77 @@ public class Demo_ArrayList {
         stringListIterator.add(newCity);
         System.out.println(newCity + " has been successfully added as a new destination");
         return true;
+    }
+
+    private static void visit(ArrayList cities) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if(cities.isEmpty()) {
+            System.out.println("No cities in the itinerary");
+            return;
+        } else {
+//            System.out.println("Now visiting " + listIterator.next());
+            System.out.println("Let's begin our journey!");
+            printMenu();
+        }
+
+        while(!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(action) {
+                case 0:
+                    System.out.println("Holiday (Vacation) over");
+                    quit = true;
+                    break;
+                case 1:
+                    if(!goingForward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the list");
+                        goingForward = false;
+                    }
+                    break;
+                case 2:
+                    if(goingForward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                        goingForward = true;
+                    }
+                    break;
+                case 3:
+                    System.out.println(cities.toString());;
+                    break;
+                case 4:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\npress ");
+        System.out.println(
+                "0 - to quit\n" +
+                        "1 - go to next city\n" +
+                        "2 - go to previous city\n" +
+                        "3 - print cities in the itinerary\n" +
+                        "4 - print menu options");
     }
 }
