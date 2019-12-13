78dd41f3d9dde253a6ed5e0f97815cfecb40fa83
jinook929 <jinook929@gmail.com>
2019-12-12 22:26:43 -0500

    Add comments

--- src/academy/learnprogramming/ListIteratorPractice.java
+++ src/academy/learnprogramming/ListIteratorPractice.java
@@ -7,22 +7,26 @@ public class ListIteratorPractice {
 
     public static void main(String[] args) {
 
+        // Create ArrayList
         ArrayList<Integer> array1 = new ArrayList<>();
 
+        // Add elements to ArrayList
         array1.add(10);
         array1.add(20);
         array1.add(30);
         array1.add(40);
         array1.add(50);
 
-        // How to make ListIterator for list
+        // Make ListIterator for list
         ListIterator<Integer> listIterator = array1.listIterator();
 
+        // Print ArrayList in forward order
         System.out.println("array1 in forward order");
         while(listIterator.hasNext()) {
             System.out.println(listIterator.next());
         }
 
+        // Print ArrayList in reverse order
         System.out.println("array1 in reverse order");
         while(listIterator.hasPrevious()) {
             System.out.println(listIterator.previous());
